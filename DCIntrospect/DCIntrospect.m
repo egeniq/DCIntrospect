6ba7d90b9978bbd8f0f2a878ddf4e56243f7232f
Ivo Jansch <ivo@egeniq.com>
2016-01-21 23:50:04 +0100

    More 64 bit fixes

--- DCIntrospect/DCIntrospect.m
+++ DCIntrospect/DCIntrospect.m
@@ -128,7 +128,10 @@ id UITextInputTraits_valueForKey(id self, SEL _cmd, NSString *key)
 		free(properties);
 	}
 	
-	IMP valueForKey = (IMP)[objc_getAssociatedObject([self class], originalValueForKeyIMPKey) pointerValue];
+    // https://github.com/adjohnson916/DCIntrospect/commit/9dabc0bbe8764092eb16d88f4b6f83bb5fe6f6d2
+    // See https://github.com/kstenerud/ObjectAL-for-iPhone/issues/73#issuecomment-64778814
+    // See http://stackoverflow.com/questions/2650190/objective-c-and-use-of-sel-imp
+    id (*valueForKey)(id, SEL, NSString*)  = (id (*)(id, SEL, NSString*))[objc_getAssociatedObject([self class], originalValueForKeyIMPKey) pointerValue];
 	if ([textInputTraitsProperties containsObject:key])
 	{
 		id textInputTraits = valueForKey(self, _cmd, @"textInputTraits");
